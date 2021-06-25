618431423d0126521f2fcf99fd32d5b35145587b
rneurink <21998181+rneurink@users.noreply.github.com>
2021-06-25 17:33:31 +0200

    Added more API functions

--- src/PCA9622.cpp
+++ src/PCA9622.cpp
@@ -1,50 +1,236 @@
 #include "PCA9622.h"
 #include "I2C_coms.h"
 
+/*----------------------- Initialisation functions --------------------------*/
+
 /**
  * @brief This function instantiates the class object
+ * 
+ * @param i2c_address The I2C address of the PCA9622 chip
+ * @param outputEnablePin The arduino pin that is connected to the ~OE pin of the PCA9266
  */
-PCA9622::PCA9622(uint8_t i2c_address, uint8_t output_enable_pin) {
+PCA9622::PCA9622(uint8_t i2c_address, uint8_t outputEnablePin) {
     _i2c_address = i2c_address;
-    _OE_pin = output_enable_pin;
+    _OE_pin = outputEnablePin;
 }
 
+/**
+ * @brief This function instantiates the class object with a specified LED configuration
+ * 
+ * @param i2c_address The I2C address of the PCA9622 chip
+ * @param outputEnablePin The arduino pin that is connected to the ~OE pin of the PCA9266
+ * @param ledConfiguration The LED configuration in which the leds are attached to the outputs. See @ref LED_Configuration
+ */
+PCA9622::PCA9622(uint8_t i2c_address, uint8_t outputEnablePin, LED_Configuration ledConfiguration) {
+    _i2c_address = i2c_address;
+    _OE_pin = outputEnablePin;
+    _led_configuration = ledConfiguration;
+}
+
+/**
+ * @brief Initializes the I2C bus and the PCA9622
+ * 
+ */
 void PCA9622::begin() {
     i2c_init();
     pinMode(_OE_pin, OUTPUT);
-    digitalWrite(_OE_pin, HIGH); // Disable outputs
+    disableOutputs();
 
-    writeRegister(PCA9622_MODE1, 0x6F);
+    softwareReset();
+
+    writeRegister(PCA9622_MODE1, 0x6E);
     delay(10);
 }
 
+/**
+ * @brief Resets the PCA9622 @warning resets all PCA9622 devices on the I2C Bus!
+ * 
+ */
 void PCA9622::softwareReset() {
     i2c_write_byte(PCA9622_I2C_SW_RESET, 0xA5, 0x5A);
+    // Wait a few microseconds for the reset to complete. Ready after the specified bus free time. (100kHz: 4.7us, 400kHz: 1.3us, 1MHz: 0.5us)
     delayMicroseconds(20);
 }
+
+
+/*----------------------- Configuration functions ---------------------------*/
+
+/**
+ * @brief Sets the output enable pin used in the library. This pin is used to turn the outputs on or off
+ * 
+ * @param outputEnablePin 
+ */
+void PCA9622::setOutputEnablePin(uint8_t outputEnablePin) {
+    _OE_pin = outputEnablePin;
+}
+
+/**
+ * @brief Sets the LED configuration acording the @ref LED_Configuration enum
+ * 
+ * @param ledConfiguration 
+ */
+void PCA9622::setLEDConfiguration(LED_Configuration ledConfiguration) {
+    _led_configuration = ledConfiguration;
+}
+
+/**
+ * @brief Sets the I2C address
+ * 
+ * @param i2c_address 
+ */
+void PCA9622::setI2CAddress(uint8_t i2c_address) {
+    _i2c_address = i2c_address;
+}
+
+void PCA9622::sleep() {
+    i2c_write_byte(_i2c_address, PCA9622_MODE1, readRegister(PCA9622_MODE1) | PCA9622_Configuration::SLEEP);
+}
+
+void PCA9622::wakeUp() {
+    i2c_write_byte(_i2c_address, PCA9622_MODE1, readRegister(PCA9622_MODE1) | PCA9622_Configuration::WAKEUP);
+    delayMicroseconds(500);
+}
+
+void PCA9622::setSubAddress1(uint8_t address, EAddressType addressType) {
+    uint8_t retVal = i2c_write_byte(getAddress(addressType), PCA9622_I2C_SUB_1, address);
+    if (retVal == 0) {
+        _i2c_address_sub_1 = address;
+    }
+}
+
+void PCA9622::setSubAddress2(uint8_t address, EAddressType addressType) {
+    uint8_t retVal = i2c_write_byte(getAddress(addressType), PCA9622_I2C_SUB_2, address);
+    if (retVal == 0) {
+        _i2c_address_sub_2 = address;
+    }
+}
+
+void PCA9622::setSubAddress3(uint8_t address, EAddressType addressType) {
+    uint8_t retVal = i2c_write_byte(getAddress(addressType), PCA9622_I2C_SUB_3, address);
+    if (retVal == 0) {
+        _i2c_address_sub_3 = address;
+    }
+}
+
+void PCA9622::setAllCallAddress(uint8_t address, EAddressType addressType) {
+    uint8_t retVal = i2c_write_byte(getAddress(addressType), PCA9622_ALL_CALL, address);
+    if (retVal == 0) {
+        _i2c_address_all_call = address;
+    }
+}
+
+void PCA9622::configure(uint8_t configuration, EAddressType addressType) {
+    i2c_write_byte(getAddress(addressType), PCA9622_MODE1, configuration);
+}
+
+void PCA9622::enableGlobalDimming(EAddressType addressType) {
+    
+}
+
+void PCA9622::enableGlobalBlinking(EAddressType addressType) {
+
+}
+
+
+/*----------------------- General control functions -------------------------*/
 
 /**
  * @brief Reads the value from the specified register
  * 
- * @param reg_addr the register address to read from
+ * @param regAddress the register address to read from
  * @return uint8_t the value from the specified register
  */
-uint8_t PCA9622::readRegister(uint8_t reg_addr) {
+uint8_t PCA9622::readRegister(uint8_t regAddress) {
     uint8_t data;
-    i2c_read_byte(_i2c_address, reg_addr, &data);
+    i2c_read_byte(_i2c_address, regAddress, &data);
     return data;
 }
 
 /**
  * @brief Writes a given value to the specified register
  * 
- * @param reg_addr the register address to write to
+ * @param regAddress the register address to write to
  * @param data the data to write to the register
  * @param addressType the I2C address type to write to 
  */
-void PCA9622::writeRegister(uint8_t reg_addr, uint8_t data, EAddressType addressType) {
-    i2c_write_byte(getAddress(addressType), reg_addr, data);
+void PCA9622::writeRegister(uint8_t regAddress, uint8_t data, EAddressType addressType) {
+    i2c_write_byte(getAddress(addressType), regAddress, data);
 }
+
+
+
+/**
+ * @brief Drives the ~OE pin low and enables the outputs of the PCA9622
+ * 
+ */
+void PCA9622::enableOutputs() {
+    digitalWrite(_OE_pin, LOW);
+}
+
+/**
+ * @brief Drives the ~OE pin high and disables the outputs of the PCA9622
+ * 
+ */
+void PCA9622::disableOutputs() {
+    digitalWrite(_OE_pin, HIGH);
+}
+
+
+
+/**
+ * @brief Sets the pwm output of the driver
+ * 
+ * @param output The output to write to from 0..15
+ * @param value The PWM value to write from 0..255
+ * @param addressType the I2C address type to write to 
+ */
+void PCA9622::setPWMOutput(uint8_t output, uint8_t value, EAddressType addressType) {
+    i2c_write_byte(getAddress(addressType), PCA9622_PWM0 + output, value);
+}
+
+/**
+ * @brief Sets all the pwm outputs of the driver
+ * 
+ * @param value The PWM value to write from 0..255
+ * @param addressType the I2C address type to write to 
+ */
+void PCA9622::setAllPWMOutputs(uint8_t value, EAddressType addressType) {
+    uint8_t buffer[16];
+    for (uint8_t i = 0; i < 16; i++) {
+        buffer[i] = value;
+    }
+    i2c_write_multi(getAddress(addressType), PCA9622_PWM0 | PCA9622_AI_INDIVIDUAL, buffer, 16);
+}
+
+/**
+ * @brief Sets the group duty cycle. 
+ * When DMBLNK is set to 0, a 190Hz fixed frequency signal is superimposed with the 97kHz individual brightness control signal.
+ * When DMBLNK is set to 1, togheter with the group frequency defines a global blinking pattern in which the value will determine the duty cycle of the ON/OFF ratio as a percentage
+ * 
+ * @param value 
+ * @param addressType 
+ */
+void PCA9622::setGroupPWM(uint8_t value, EAddressType addressType) {
+    i2c_write_byte(getAddress(addressType), PCA9622_GRPPWM, value);
+}
+
+uint16_t PCA9622::setGroupFrequency(uint16_t ms, EAddressType addressType) {
+    if (ms < 42) ms = 42;
+    if (ms > 10666) ms = 10666;
+    uint8_t regValue = (uint8_t)((((uint32_t)ms * 24) - 1000) / 1000);
+    uint16_t actualValue = (uint16_t)(((regValue + 1) * 1000) / 24);
+
+    i2c_write_byte(getAddress(addressType), PCA9622_GRPFREQ, regValue);
+    return actualValue;
+}
+
+
+/*----------------------- RGB control functions -----------------------------*/
+
+
+
+
+/*------------------------- Helper functions --------------------------------*/
 
 /*
  *  PRIVATE
@@ -78,4 +264,14 @@ uint8_t PCA9622::getAddress(EAddressType addressType) {
         break;
     }
     return i2c_address;
+}
+
+
+
+
+void PCA9622::test() {
+    uint8_t buffer[] = {0x10, 0x20, 0x30};
+    i2c_write_multi(_i2c_address, 0xE2, buffer, 3);
+    delay(10);
+    i2c_read_multi(_i2c_address, 0xE0, buffer, 3);
 }
--- src/PCA9622.h
+++ src/PCA9622.h
@@ -5,7 +5,7 @@
 #include <Wire.h>
 
 #define PCA9622_I2C_ALL_CALL    0xE0
-#define PCA9622_I2C_SW_RESET    0x0C
+#define PCA9622_I2C_SW_RESET    0x06
 #define PCA9622_I2C_SUB_1       0xE2
 #define PCA9622_I2C_SUB_2       0xE4
 #define PCA9622_I2C_SUB_3       0xE8
@@ -14,6 +14,7 @@
 #define PCA9622_MODE1           0x00 // Mode register 1
 #define PCA9622_MODE2           0x01 // Mode register 2
 #define PCA9622_PWM0            0x02 // Brightness control LED0
+// PWM1 on 0x03, PWM2 on 0x04 etc... to PWM15 on 0x11
 #define PCA9622_GRPPWM          0x12 // Group duty cycle control
 #define PCA9622_GRPFREQ         0x13 // Group frequency
 #define PCA9622_LED_OUT0        0x14 // LED output state 0
@@ -25,6 +26,35 @@
 #define PCA9622_SUB_ADR3        0x1A // I2C subaddress 3
 #define PCA9622_ALL_CALL        0x1B // I2C All call address
 
+// Auto increment flags
+#define PCA9622_NO_AI           0x00 // No auto increment
+#define PCA9622_AI_ALL          0x80 // Auto increment all registers. roll over at 0x1B to 0x00
+#define PCA9622_AI_INDIVIDUAL   0xA0 // Auto increment individual brightness only. roll over at 0x11 to 0x02
+#define PCA9622_AI_GLOBAL       0xC0 // Auto increment global control registers only. roll over at 0x13 to 0x12 
+#define PCA9622_AI_INDI_GLOBAL  0xE0 // Auto increment individual and global registers only. roll over at 0x13 to 0x02
+
+enum LED_Configuration {
+    RGB,
+    GRB,
+    BGR,
+    RBG,
+    GBR,
+    BRG,
+    RGBA,
+    GRBA,
+    BGRA,
+    RBGA,
+    GBRA,
+    BRGA,
+};
+
+enum LED_State {
+    OFF = 0x00,
+    ON = 0x01,
+    PWM_CONTROL = 0x02,
+    PWM_AND_GROUP_CONTROL = 0x03
+};
+
 enum EAddressType {
     Normal,
     AllCall,
@@ -33,16 +63,57 @@ enum EAddressType {
     SubCall3
 };
 
+enum PCA9622_Configuration {
+    ALL_CALL_OFF = 0 << 0,
+    ALL_CALL_ON = 1 << 0,
+    SUB_3_OFF = 0 << 1,
+    SUB_3_ON = 1 << 1,
+    SUB_2_OFF = 0 << 2,
+    SUB_2_ON = 1 << 2,
+    SUB_1_OFF = 0 << 3,
+    SUB_1_ON = 1 << 3,
+    SLEEP = 1 << 4,
+    WAKEUP = 0 << 4
+};
+
 class PCA9622
 {
     public:
-        PCA9622(uint8_t i2c_address, uint8_t output_enable_pin); // Constructor
+        PCA9622(uint8_t i2c_address, uint8_t outputEnablePin); // Constructor
+        PCA9622(uint8_t i2c_address, uint8_t outputEnablePin, LED_Configuration ledConfiguration); // Constructor with specific led configuration
 
         void begin();
         void softwareReset();
 
-        uint8_t readRegister(uint8_t reg_addr);
-        void writeRegister(uint8_t reg_addr, uint8_t data, EAddressType addressType = EAddressType::Normal);
+        void setOutputEnablePin(uint8_t outputEnablePin);
+        void setLEDConfiguration(LED_Configuration ledConfiguration);
+        void setI2CAddress(uint8_t i2c_address);
+
+        void sleep();
+        void wakeUp();
+        void setSubAddress1(uint8_t address, EAddressType addressType = EAddressType::Normal);
+        void setSubAddress2(uint8_t address, EAddressType addressType = EAddressType::Normal);
+        void setSubAddress3(uint8_t address, EAddressType addressType = EAddressType::Normal);
+        void setAllCallAddress(uint8_t address, EAddressType addressType = EAddressType::Normal);
+        void configure(uint8_t configuration, EAddressType addressType = EAddressType::Normal);
+        void enableGlobalDimming(EAddressType addressType = EAddressType::Normal);
+        void enableGlobalBlinking(EAddressType addressType = EAddressType::Normal);
+
+        uint8_t readRegister(uint8_t regAddress);
+        void writeRegister(uint8_t regAddress, uint8_t data, EAddressType addressType = EAddressType::Normal);
+
+        void writeMultiRegister(uint8_t startAddress, uint8_t *data, uint8_t count, EAddressType addressType = EAddressType::Normal);
+
+        void enableOutputs();
+        void disableOutputs();
+
+        void setPWMOutput(uint8_t output, uint8_t value, EAddressType addressType = EAddressType::Normal);
+        void setAllPWMOutputs(uint8_t value, EAddressType addressType = EAddressType::Normal);
+
+        void setGroupPWM(uint8_t value, EAddressType addressType = EAddressType::Normal);
+        uint16_t setGroupFrequency(uint16_t ms, EAddressType addressType = EAddressType::Normal);
+
+        void test();
 
     protected:
     private:
@@ -55,7 +126,10 @@ class PCA9622
         uint8_t _i2c_address_sub_2 = PCA9622_I2C_SUB_2;
         uint8_t _i2c_address_sub_3 = PCA9622_I2C_SUB_3;
 
+        LED_Configuration _led_configuration = RGB;
+
         uint8_t getAddress(EAddressType addressType);
+        uint8_t fillLEDbuffer(uint8_t *buffer, LED_Configuration ledConfiguration);
 };
 
 #endif
